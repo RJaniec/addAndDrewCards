7c870f616802180bd4d359905e4045425b7d8692
RJaniec <rafael.janiec@outlook.com>
2022-06-28 12:22:26 +0200

    Initial Commit

---
+++ arrays_challenge/arrays_challengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  arrays_challengeApp.swift
+//  arrays_challenge
+//
+//  Created by Rafał Janiec on 28/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct arrays_challengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
